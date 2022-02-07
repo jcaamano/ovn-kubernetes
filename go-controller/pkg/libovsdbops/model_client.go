db051c66fd1aaeb7dc7de178df438ff05ec14f4c
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2022-02-07 12:48:32 +0000

    libovsdb: cleanup ModelClient usage for ACLs
    For ACLs:
    
    * remove ModelClient usage outside of libovsdbops
    * refactor libovsdbops to use ModelCLient
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/libovsdbops/model_client.go
+++ go-controller/pkg/libovsdbops/model_client.go
@@ -315,12 +315,14 @@ func (m *ModelClient) create(opModel *OperationModel) ([]ovsdb.Operation, error)
 			return nil, err
 		}
 		ops = append(ops, waitOps...)
-	} else if info, err := m.client.Cache().DatabaseModel().NewModelInfo(opModel.Model); err == nil {
-		if name, err := info.FieldByColumn("name"); err == nil {
-			objName := getString(name)
-			if len(objName) > 0 {
-				klog.Warningf("OVSDB Create operation detected without setting opModel Name. Name: %s, %#v",
-					objName, info)
+	} else if cache := m.client.Cache(); cache != nil { // cache might be nil if disconnected
+		if info, err := cache.DatabaseModel().NewModelInfo(opModel.Model); err == nil {
+			if name, err := info.FieldByColumn("name"); err == nil {
+				objName := getString(name)
+				if len(objName) > 0 {
+					klog.Warningf("OVSDB Create operation detected without setting opModel Name. Name: %s, %#v",
+						objName, info)
+				}
 			}
 		}
 	}
