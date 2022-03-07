91822859b1a2aecc6f3a6b40bef03b1fa924cd08
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2022-03-07 17:56:37 +0000

    libovsdb: cleanup ModelClient usage for NB & SB Global
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/libovsdbops/nb_global.go
+++ go-controller/pkg/libovsdbops/nb_global.go
@@ -1,47 +1,48 @@
 package libovsdbops
 
 import (
-	"context"
-	"fmt"
-
 	libovsdbclient "github.com/ovn-org/libovsdb/client"
 
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/nbdb"
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 )
 
-// findNBGlobal returns the NBGlobal table entry
-func FindNBGlobal(nbClient libovsdbclient.Client) (*nbdb.NBGlobal, error) {
-	nbGlobal := []nbdb.NBGlobal{}
-	ctx, cancel := context.WithTimeout(context.Background(), types.OVSDBTimeout)
-	defer cancel()
-	err := nbClient.List(ctx, &nbGlobal)
-	if err != nil {
-		return nil, fmt.Errorf("failed listing nbGlobal table entires err: %v", err)
-	}
-	// We should error if the nbGlobal table entry does not exist
-	if len(nbGlobal) == 0 {
-		return nil, libovsdbclient.ErrNotFound
-	}
-	// The nbGlobal table should only have one row
-	if len(nbGlobal) != 1 {
-		return nil, fmt.Errorf("multible NBGlobal rows found")
+// GetNBGlobal looks up the NB Global entry from the cache
+func GetNBGlobal(nbClient libovsdbclient.Client) (*nbdb.NBGlobal, error) {
+	found := []*nbdb.NBGlobal{}
+	opModel := OperationModel{
+		ModelPredicate: func(item *nbdb.NBGlobal) bool { return true },
+		ExistingResult: &found,
+		OnModelUpdates: nil, // no update
+		ErrNotFound:    true,
+		BulkOp:         false,
 	}
 
-	return &nbGlobal[0], nil
+	m := NewModelClient(nbClient)
+	_, err := m.CreateOrUpdate(opModel)
+	return found[0], err
 }
 
-// UpdateNBGlobalOptions updates the options on the NBGlobal table, adding any newly specified options in the process
-func UpdateNBGlobalOptions(nbClient libovsdbclient.Client, options map[string]string) error {
+// UpdateNBGlobalSetOptions sets options on the NB Global entry adding any
+// missing, removing the ones set to an empty value and updating existing
+func UpdateNBGlobalSetOptions(nbClient libovsdbclient.Client, nbGlobal *nbdb.NBGlobal) error {
+	options := nbGlobal.Options
 	// find the nbGlobal table's UUID, we don't have any other way to reliably look this table entry since it can
 	// only be indexed by UUID
-	nbGlobal, err := FindNBGlobal(nbClient)
+	nbGlobal, err := GetNBGlobal(nbClient)
 	if err != nil {
 		return err
 	}
 
+	if nbGlobal.Options == nil {
+		nbGlobal.Options = map[string]string{}
+	}
+
 	for k, v := range options {
-		nbGlobal.Options[k] = v
+		if v == "" {
+			delete(nbGlobal.Options, k)
+		} else {
+			nbGlobal.Options[k] = v
+		}
 	}
 
 	// Update the options column in the nbGlobal entry since we already performed a lookup
@@ -51,12 +52,10 @@ func UpdateNBGlobalOptions(nbClient libovsdbclient.Client, options map[string]st
 			&nbGlobal.Options,
 		},
 		ErrNotFound: true,
+		BulkOp:      false,
 	}
 
 	m := NewModelClient(nbClient)
-	if _, err := m.CreateOrUpdate(opModel); err != nil {
-		return fmt.Errorf("error while updating NBGlobal Options: %v error %v", options, err)
-	}
-
-	return nil
+	_, err = m.CreateOrUpdate(opModel)
+	return err
 }
